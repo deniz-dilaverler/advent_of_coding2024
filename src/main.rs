e7d8e1815dce3ac7382afd4b3c9e9c88ca706de4
Deniz Mert Dilaverler <deniz.dilaverler@rwth-aachen.de>
2024-12-04 13:01:10 +0100

    Solution1.2

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ mod solution1;
 
 fn main() {
     let (l1, l2) = solution1::read_question1_input("./input1.txt");
-    let result = solution1::solve_question1(l1, l2);
+    let result = solution1::solve_question1_2(l1, l2);
     println!("{}", result);
 }
--- src/solution1.rs
+++ src/solution1.rs
@@ -28,3 +28,34 @@ pub fn solve_question1(mut l1: Vec<i32>, mut l2: Vec<i32>) -> i32 {
     }
     return count;
 }
+
+pub fn solve_question1_2(mut l1: Vec<i32>, mut l2: Vec<i32>) -> i32 {
+    let mut similarity_score = 0;
+    l1.sort();
+    l2.sort();
+
+    let mut last = (-1, -1);
+    let mut ptr2 = 0;
+    for val1 in l1.iter() {
+        if *val1 == last.0 {
+            similarity_score += last.1;
+            continue;
+        }
+        last.0 = *val1;
+
+        let mut count = 0;
+        while ptr2 < l2.len() && l2[ptr2] < *val1 {
+            ptr2 += 1;
+        }
+
+        while ptr2 < l2.len() && l2[ptr2] == *val1 {
+            count += 1;
+            ptr2 += 1;
+        }
+
+        last.1 = count * val1;
+        similarity_score += last.1;
+    }
+
+    return similarity_score;
+}
